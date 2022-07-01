357b8ec45e23c5c0a34af4470d06ad69ae465350
Santos <sanmartgon@gmail.com>
2022-07-01 19:37:54 +0200

    voy avanzando tercer selector

--- src/app/paises/pages/selector-page/selector-page.component.ts
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PaisesService } from '../../services/paises.service';
+import { Pais } from '../../interfaces/paises.interface';
+import { switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-selector-page',
@@ -10,18 +12,58 @@ import { PaisesService } from '../../services/paises.service';
 export class SelectorPageComponent implements OnInit {
   miFormulario: FormGroup = this.fb.group({
     region: ['', Validators.required],
+    pais: ['', Validators.required],
+    frontera: ['', Validators.required],
   });
 
   private _regiones: string[] = [];
+  private _paises: Pais[] = [];
+  private _fronteras: string[] = [];
 
   get regiones(): string[] {
     return this._regiones;
+  }
+
+  get paises(): Pais[] {
+    return this._paises;
+  }
+
+  get fronteras() {
+    return this._fronteras;
   }
 
   constructor(private fb: FormBuilder, private paisService: PaisesService) {}
 
   ngOnInit(): void {
     this._regiones = this.paisService.regiones;
+
+    // Cuando cambie la región
+    this.miFormulario
+      .get<string>('region')
+      ?.valueChanges.pipe(
+        tap((_) => {
+          this.miFormulario.controls['pais'].setValue('');
+        }),
+        switchMap((region) => this.paisService.getPaisesPorRegion(region))
+      )
+      .subscribe((paises) => {
+        this._paises = paises;
+      });
+    // Cuando cambie el Pais
+    this.miFormulario
+      .get<string>('pais')
+      ?.valueChanges.pipe(
+        tap((_) => {
+          this.miFormulario.controls['frontera']?.setValue('');
+        }),
+        switchMap((pais) => {
+          return pais ? this.paisService.getFronterasPorPais(pais) : [];
+        })
+      )
+      .subscribe((fronteras) => {
+        this._fronteras = fronteras;
+        console.log(this._fronteras);
+      });
   }
 
   guardar() {
--- src/app/paises/services/paises.service.ts
+++ src/app/paises/services/paises.service.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Pais } from '../interfaces/paises.interface';
+import { map, Observable } from 'rxjs';
+import { Pais2 } from '../interfaces/pais2.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -13,9 +16,23 @@ export class PaisesService {
     'oceania',
   ];
 
+  private _url: string = 'https://restcountries.com/v3.1/';
+
   get regiones(): string[] {
     return [...this._regiones];
   }
 
   constructor(private http: HttpClient) {}
+
+  getPaisesPorRegion(region: string): Observable<Pais[]> {
+    return this.http.get<Pais[]>(`${this._url}/region/${region}`);
+  }
+
+  getFronterasPorPais(pais: string) {
+    return this.http.get<Pais2[]>(`${this._url}name/${pais}`).pipe(
+      map((pais) => {
+        return pais[0].borders ? pais[0].borders : [];
+      })
+    );
+  }
 }
